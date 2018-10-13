26466d7ead0baa4342b8c5c50b0aa0c21dd83925
Mikhail <43931801+MikhailOnuchin@users.noreply.github.com>
2018-10-13 22:45:34 +0300

    small refactoring in 2015-16_N4

--- vosh_school/2015-16_N4.py
+++ vosh_school/2015-16_N4.py
@@ -9,16 +9,17 @@ for digit in my_int:
     digit_sum += int(digit)
     digits.append(int(digit))
 addendum = 3 - (digit_sum % 3)
-changes = 0
+found = False
 for i in range(len(digits)):
     new_digit = digits[i] + addendum
     if new_digit > 9:
         continue
     else:
-        changes = 1
+        found = True
         digits[i] = new_digit
         break
-if changes == 0:
+if not found:
+    assert digits[-1] > 6
     if digit_sum % 3 == 0:
         digits[-1] -= 3
     else:
