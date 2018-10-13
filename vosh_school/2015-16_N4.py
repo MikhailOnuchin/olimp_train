2c0fc6522c51969975f839f5c12eb51fe958e0dc
Mikhail <43931801+MikhailOnuchin@users.noreply.github.com>
2018-10-13 20:01:42 +0300

    solved N4

--- vosh_school/2015-16_N4.py
+++ vosh_school/2015-16_N4.py
@@ -2,3 +2,26 @@
 # максимально возможным. В исходном числе нужно обязательно изменить одну цифру, даже если исходное число уже делилось на 3
 
 
+my_int = input()
+digits = []
+digit_sum = 0
+for digit in my_int:
+    digit_sum += int(digit)
+    digits.append(int(digit))
+addendum = 3 - (digit_sum % 3)
+changes = 0
+for i in range(len(digits)):
+    new_digit = digits[i] + addendum
+    if new_digit > 9:
+        continue
+    else:
+        changes = 1
+        digits[i] = new_digit
+        break
+if changes == 0:
+    if digit_sum % 3 == 0:
+        digits[-1] -= 3
+    else:
+        digits[-1] -= digit_sum % 3
+for digit in digits:
+    print(digit, end='')
