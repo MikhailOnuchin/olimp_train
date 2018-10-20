e9b5e78aedd017dad3d100b5cafef18e47f2cc45
Mikhail <43931801+MikhailOnuchin@users.noreply.github.com>
2018-10-20 19:22:28 +0300

    solved N4

---
+++ vosh_municipal/2017-18/N4.py
@@ -0,0 +1,45 @@
+
+
+def elevator(building, el_cap):
+    time = 0
+    el_pos = 0
+    el_free = el_cap
+    for floor in range(1, len(building) + 1):
+        people = building[floor-1]
+        if  people > 0:
+            if el_pos < floor:
+                el_pos = floor
+            if people < el_free:
+                el_free -= people
+                continue
+            time += 2*el_pos
+            people -= el_free
+            el_free = el_cap
+            time += 2*el_pos*(people//el_cap)
+            el_free -= people % el_cap
+    return time
+
+
+def test1():
+    assert elevator([3,0,1], 2) == 8
+
+
+def test2():
+    building = []
+    for i in range(100000):
+        building.append(1000000000)
+    elevator(building, 1000000000)
+
+
+def main():
+    k = int(input())
+    building = []
+    for i in range(int(input())):
+        building.append(int(input()))
+    print(elevator(building, k))
+
+
+if __name__ == '__main__':
+    #main()
+    test1()
+    test2()
