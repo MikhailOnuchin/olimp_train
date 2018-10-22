7806e159151c7e428a645f0500b51bf62e53b6b7
Mikhail <43931801+MikhailOnuchin@users.noreply.github.com>
2018-10-22 19:53:19 +0300

    finished refactoring N3 for municipal

--- vosh_municipal/2017-18/N3.py
+++ vosh_municipal/2017-18/N3.py
@@ -1,18 +1,35 @@
 
 
-a = input()
-b = input()
-c = input()
-timea = int(a[:2])*3600 + int(a[3:5])*60 + int(a[6:])
-timeb = int(b[:2])*3600 + int(b[3:5])*60 + int(b[6:])
-timec = int(c[:2])*3600 + int(c[3:5])*60 + int(c[6:])
-if timea > timec:
-    timec += 86400
-delta = (timec-timea)/2
-timeb += delta
-timeb %= 86400
-hours = int(timeb//3600 + 0.5)
-timeb %= 3600
-mins = int(timeb//60 + 0.5)
-secs = int(timeb%60 +0.5)
-print('%02d:%02d:%02d' % (hours, mins, secs))
+def sntp(a, b, c):
+    timea = int(a[:2]) * 3600 + int(a[3:5]) * 60 + int(a[6:])
+    timeb = int(b[:2]) * 3600 + int(b[3:5]) * 60 + int(b[6:])
+    timec = int(c[:2]) * 3600 + int(c[3:5]) * 60 + int(c[6:])
+    if timea > timec:
+        timec += 86400
+    delta = (timec-timea) / 2
+    timeb = int(timeb + delta + 0.5) % 86400
+    hours = timeb // 3600
+    mins = (timeb % 3600) // 60
+    secs = timeb % 60
+    return '%02d:%02d:%02d' % (hours, mins, secs)
+
+
+def test1():
+    assert sntp('15:01:00', '18:09:45', '15:01:40') == '18:10:05'
+
+
+def test2():
+    assert sntp('16:00:40', '00:00:00', '16:01:30') == '00:00:25'
+
+
+def main():
+    a = int(input())
+    b = int(input())
+    c = int(input())
+    print(sntp(a, b, c))
+
+
+if __name__ == '__main__':
+    test1()
+    test2()
+    #main()
